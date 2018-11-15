3052fdda3f9965f7c61ad5aff5f3b75c2a30ca48
Jithin Babu <jithinbabu707@gmail.com>
2018-11-15 21:45:20 +0530

    Update ContinuousSubArray.java

--- DynamicProgramming/ContinuousSubArray.java
+++ DynamicProgramming/ContinuousSubArray.java
@@ -1,6 +1,15 @@
-[LeetCode :523](https://leetcode.com/problems/continuous-subarray-sum/description/)
+ /* Leetcode : 523. Continuous Subarray Sum
 
-                class Solution {
+  Given a list of non-negative numbers and a target integer k, write a function to check if the array has a continuous subarray of
+  size at least 2 that sums up to the multiple of k, that is, sums up to n*k where n is also an integer.
+
+Example 1:
+
+Input: [23, 2, 4, 6, 7],  k=6
+Output: True
+Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
+ */
+         class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
          LinkedList<Integer> queue = new LinkedList<Integer>();
           k =Math.abs(k);

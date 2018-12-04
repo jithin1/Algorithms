659f39053908f82db5417b40cdec4dea5539fc98
Jithin Babu <jithinbabu707@gmail.com>
2018-12-05 00:21:38 +0530

    Update PowerOf2.java

--- BitAlgorithms/PowerOf2.java
+++ BitAlgorithms/PowerOf2.java
@@ -3,7 +3,7 @@ class Solution {
   public boolean isPowerOfTwo(int n) {
  
        
-        if( n <=0 || (n & (n-1)) != 0)
+        if( n <=0 || (n & (n-1)) != 0)  // n & (n-1) is used to check whether there is only one power of 2
        
                 return false;
         

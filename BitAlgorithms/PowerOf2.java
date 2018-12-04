ad7137005283ee48913064117a18df492d49b5ba
jithin1 <jithinbabu707@gmail.com>
2018-12-05 00:19:45 +0530

    new file

---
+++ BitAlgorithms/PowerOf2.java
@@ -0,0 +1,15 @@
+class Solution {
+    
+  public boolean isPowerOfTwo(int n) {
+ 
+       
+        if( n <=0 || (n & (n-1)) != 0)
+       
+                return false;
+        
+
+             return true;
+ 
+         }
+   
+}

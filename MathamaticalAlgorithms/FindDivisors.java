5632f6ec5091de466f84dc35725aa8c3de506c21
Jithin Babu <jithinbabu707@gmail.com>
2020-10-29 21:18:12 +0530

    Create FindDivisors.java

---
+++ MathamaticalAlgorithms/FindDivisors.java
@@ -0,0 +1,26 @@
+private static void computePrimeFactors(){
+          for(int i=0;i<primes.length;i++)
+            primes[i] = i;
+          
+          for(int p=2;p*p<max;p++){
+               if(primes[p] == p){
+                    for(int i=p*2;i<max;i+=p)
+                     primes[i] = Math.min(primes[i],p);
+               }
+          }
+     }
+     
+     // don't call this method when  n == 1 or n == 2
+     private static int findDivisors(int n){
+        int divisors = 1;
+        while(n > 1){
+             int count = 0;
+             int fact = primes[n];
+             while( n > 1 && n%fact == 0){
+                  count++;
+                  n/=fact;
+             }
+             divisors*=(count+1);
+        }
+        return divisors;
+     }
